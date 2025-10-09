423012d1a4cd60932fb86f3a866e0f066a8aeab0
VictoriaStudios <88744359+VictoriaStudios@users.noreply.github.com>
2025-10-09 18:52:32 -0500

    Can now delete the account

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -9,7 +9,7 @@ const AboutUs: React.FC = () => {
       <p>This is a placeholder for the About Us page.</p>
       <h1>Account deletion</h1>
       <p>If you want your data deleted from our servers, write an email from the gmail account you used in game. We will contact you with a confirmation mail.</p>
-      <a href="mailto:marcel.bruna@epistula-electronica.de?subject=Account%20Deletion&body=Hi,<br/> I%20would%20like%20to%20have%20my%20account%20deleted." className={styles.button}>Delete Account</a>
+      <a href="mailto:marcel.bruna@epistula-electronica.de?subject=Account%20Deletion&body=Hi! I would like to have my account deleted." className={styles.button}>Delete Account</a>
     </div>
   );
 };
