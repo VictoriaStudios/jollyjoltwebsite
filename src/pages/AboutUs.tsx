e717dd0b167a6ea78d42a9bb303bac1de6e2ff0b
VictoriaStudios <88744359+VictoriaStudios@users.noreply.github.com>
2025-10-09 18:40:29 -0500

    Added about us account deletion

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -1,11 +1,15 @@
 
 import React from 'react';
+import styles from './AboutUs.module.css';
 
 const AboutUs: React.FC = () => {
   return (
     <div>
       <h1>About Us</h1>
       <p>This is a placeholder for the About Us page.</p>
+      <h1>Account deletion</h1>
+      <p>If you want your data deleted from our servers, write an email from the gmail account you used in game. We will contact you with a confirmation mail.</p>
+      <a href="mailto:marcel.bruna@epistula-electronica.de?subject=Account%20Deletion&body=Hi,<br/> I%20would%20like%20to%20have%20my%20account%20deleted." className={styles.button}>Delete Account</a>
     </div>
   );
 };
